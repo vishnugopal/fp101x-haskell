4c379a9dbcda7d062141bb43946c3ee2c0383398
Vishnu Gopal <g.vishnu@gmail.com>
2014-11-05 00:52:31 +0530

    Committing lab2

--- lecture5.hs
+++ lecture5.hs
@@ -1,13 +1,17 @@
 -- Lecture 5
 
+import Prelude hiding (drop)
+
 -- Rewriting correct fibonacci from Hutton p.68
 -- because Haskell seems to have lost n + 1 patterns
 fibonacci :: Int -> Int
-fibonacci 0       = 0
-fibonacci 1       = 1
-fibonacci n       = fibonacci (n - 1) + fibonacci (n - 2)
+fibonacci 0 = 0
+fibonacci 1 = 1
+fibonacci n = fibonacci (n - 1) + fibonacci (n - 2)
 
-
-
-
+-- Rewriting drop at Hutton p.73
+drop :: Integral b => b -> [a] -> [a]
+drop 0 xs      = xs
+drop _ []      = []
+drop n (_: xs) = drop (n - 1) xs
 
